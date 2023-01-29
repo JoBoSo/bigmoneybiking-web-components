79f2339d224900022b9eebcb382852f1608428a8
JoBoSo <jamesbscott@hotmail.ca>
2023-01-28 19:19:06 -0500

    added content

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ class Header extends HTMLElement {
   constructor() {
     super();
     this.title = 'Big Money Bicycle Touring';
-    this.subtitle = "Rise & Shine";
+    this.subtitle = "M";
   }
 
   connectedCallback() {
