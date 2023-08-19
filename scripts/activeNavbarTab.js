a5e1a3b5f1d501157de288e71f97bdcca9fa6b7c
JoBoSo <jamesbscott@hotmail.ca>
2023-08-19 13:12:06 -0700

    active navbar tab should now be colored on initial load for url / (as compared to /index.html)

--- scripts/activeNavbarTab.js
+++ scripts/activeNavbarTab.js
@@ -4,7 +4,7 @@ $(function activeTab(){
     if ($(this).prop('href') == window.location.href) {
       $(this).addClass('active'); $(this).parents('my-nav').addClass('active');
     }
-    // bigmoneybiking.ca initially loads as "https://bigmoneybiking.ca/"
+    // bigmoneybiking.ca initially loads index.html as "https://bigmoneybiking.ca/"
     else if ($(this).prop('pathname') == '/index.html' && window.location.pathname == '/') {
       $(this).addClass('active'); $(this).parents('my-nav').addClass('active');
     }
