e8fb34deca890b41921806db8c2511e4250aa195
JoBoSo <jamesbscott@hotmail.ca>
2025-04-30 15:06:45 -0700

    updated instagram and linkedin urls

--- components/Footer.js
+++ components/Footer.js
@@ -41,9 +41,9 @@ class Footer extends HTMLElement {
         <div class="social-footer-icons">
           <a href="https://github.com/JoBoSo"><i class="fa-brands fa-github" aria-hidden="true"></i></a>
           <a href="https://www.youtube.com/@bigmoneybiking618/videos"><i class="fa-brands fa-youtube" aria-hidden="true"></i></a>
-          <a href="https://www.instagram.com/big_money_biking/"><i class="fa-brands fa-instagram" aria-hidden="true"></i></a>
+          <a href="https://www.instagram.com/bigmoneybiking/"><i class="fa-brands fa-instagram" aria-hidden="true"></i></a>
           <a href="https://www.strava.com/athletes/6648947"><i class="fa-brands fa-strava" aria-hidden="true"></i></a>
-          <a href="https://www.linkedin.com/in/james-scott-the-third/"><i class="fa-brands fa-linkedin" aria-hidden="true"></i></a>
+          <a href="https://www.linkedin.com/in/jbscott/"><i class="fa-brands fa-linkedin" aria-hidden="true"></i></a>
         </div>
       </footer>
     `;
--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -61,7 +61,7 @@ class Navbar extends HTMLElement {
           <a href="${this.root}hikes.html"><b>Hikes</b></a>
           <a href="${this.root}dashboard.html"><b>Dashboard</b></a>
           <a href="${this.root}blog.html"><b>Blog</b></a>
-          <a style="font-size: 12pt" href="https://www.instagram.com/big_money_biking/"><i class="fa-brands fa-instagram" aria-hidden="true"></i></a>
+          <a style="font-size: 12pt" href="https://www.instagram.com/bigmoneybiking/"><i class="fa-brands fa-instagram" aria-hidden="true"></i></a>
           <a style="margin-left: auto;" href="${this.root}subscribe.html"><i><b>Subscribe</b></i></a>
           </a>
         </div>
