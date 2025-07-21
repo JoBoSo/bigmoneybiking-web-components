2064276ce44072b0e4be8e68d66c850610cf2ce0
JoBoSo <jamesbscott@hotmail.ca>
2025-07-20 21:04:16 -0600

    updated header and body backgrounds

--- components/Header.js
+++ components/Header.js
@@ -8,8 +8,8 @@ class Header extends HTMLElement {
       <style>
 
       #bmb-header {
-        background: #ffc272;
-        background-image: url('/components/sunset.jpg');
+        /* background: #ffc272; */
+        background-image: url('/images/header-bg.jpg');
         width: 100%;
         background-position: 50% 7%;
         background-repeat: no-repeat;
@@ -18,13 +18,13 @@ class Header extends HTMLElement {
       }
     
       #bmb-title {
-        color: #0065d1;
+        color:rgb(0, 123, 255);
         font-family: 'Pacifico', Arial, Helvetica, sans-serif;
         font-size: 22pt;
         text-align: center;
         margin-bottom: 0;
-        text-shadow: 0.5px 0.5px 15px white;
-        -webkit-text-stroke: 0.3px white; /* width and color */
+        text-shadow: -1px 3px 10px white;
+        -webkit-text-stroke: 0.4px white; /* width and color */
       }
     
       #bmb-phrase {
@@ -33,7 +33,7 @@ class Header extends HTMLElement {
         /* font-size: 18pt; */
         text-align: center;
         margin-bottom: 0;
-        color: #ffc272; /*hide*/
+        color: rgb(0, 123, 255); /*hide*/
         font-size: 5pt;
       }
 
@@ -43,7 +43,7 @@ class Header extends HTMLElement {
     this.innerHTML = style + `
       <div id="bmb-header">
         <h1 id="bmb-title">Big Money Biking</h1>
-        <p id="bmb-phrase">M</p>
+        <p id="bmb-phrase">.</p>
       </div>
     `
   }
